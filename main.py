1c63a1004f3a32950bf43d4c94f015b7fb308331
Franco904 <francostavares2003@gmail.com>
2025-06-04 20:20:32 -0300

    arruma imports

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import numpy as np # pip install numpy
-from src.neural_network import NeuralNetwork, ProblemType, Activation, Loss
+import numpy as np  # pip install numpy
+
+from src.enums.activation import Activation
+from src.enums.loss import Loss
+from src.enums.problem_type import ProblemType
+from src.neural_network import NeuralNetwork
 
 
 def main():
-    input_layer_size = 9 # arbitrary
+    input_layer_size = 9  # arbitrary
 
     neural_network = NeuralNetwork(
         problem_type=ProblemType.BinaryClassification.value,
@@ -11,7 +15,7 @@ def main():
         hidden_layers_sizes=[10, 10],
         output_layer_size=9,
         activation=Activation.Sigmoid.value,
-        loss=Loss.BinaryCrossEntropy.value,
+        loss=Loss.CrossEntropy.value,
     )
 
     inputs_1: list[float] = list(np.random.uniform(low=1, high=101, size=input_layer_size))
