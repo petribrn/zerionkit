664dbc9a4a274a5a695b9717f7773095b25f39d7
Bruno Petri <brunoopetri@gmail.com>
2025-06-08 12:08:17 -0300

    fix: remove unused enums

--- src/enums/loss.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class LossEnum(Enum):
-    Mse = 0  # mean square error
-    CrossEntropy = 1
--- src/enums/problem_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ProblemType(Enum):
-    Regression = 0
-    BinaryClassification = 1
-    MulticlassClassification = 2
