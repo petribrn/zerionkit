2ab5ca06f9074dd63f7e42bc163006a1aac42a7a
Bruno Petri <brunoopetri@gmail.com>
2025-06-16 19:12:37 -0300

    fix: column names to lowercase

--- data/multi_class/students_data_treatment.py
+++ data/multi_class/students_data_treatment.py
@@ -4,4 +4,9 @@ df = pd.read_csv('students.csv')
 
 df['Target'] = df['Target'].map({'Graduate': 1, 'Enrolled': 0, 'Dropout': 2})
 
+columns_to_rename = {}
+for column in df.columns.values.tolist():
+    columns_to_rename[column] = column.lower().strip()
+
+df.rename(columns=columns_to_rename, inplace=True)
 df.to_csv('students_converted.csv', index=False)
