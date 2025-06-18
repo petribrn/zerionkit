4c8c1a13c60caae24cb2fd6f35228e3bde2b0984
Carolina Pacheco <90946121+carolinapsil@users.noreply.github.com>
2025-06-17 23:07:49 -0300

    feat: one hot encoding

--- data/regression/bike_data_treatment.py
+++ data/regression/bike_data_treatment.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('bike.csv')
 
-df = df.drop(columns=['instant', 'dteday'])
+df = df.drop(columns=['instant', 'dteday', 'yr'])
+
+df = pd.get_dummies(df, columns=['season', 'mnth', 'hr' ,'weekday', 'weathersit']).astype(int)
+
+df = df[[col for col in df.columns if col not in ['casual', 'registered', 'cnt']]+ ['casual', 'registered', 'cnt']]
 
 df.to_csv('bike_converted.csv', index=False)
