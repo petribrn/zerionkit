0b10677f687487f56fc8958fbaa2f7a73125f8e1
Carolina Pacheco <carolina.pacheco@bridge.ufsc.br>
2025-06-29 17:36:45 -0300

    fix: data_handler to use slices instead of just the complete dataset

--- src/data_handler.py
+++ src/data_handler.py
@@ -9,29 +9,30 @@ class DataHandler:
     def __init__(
             self,
             problem_type: Literal['regression', 'binary_class', 'multi_class'],
-            data_source_filename: str,
+            dataset_name: Literal['mushroom', 'students', 'bike'],
             y_target_columns: str | list[str],
-            auto_process: bool = False,
-            persist_to_csv: bool = False,
+            dataset_type: Literal['complete', 'training', 'validation', 'test'],
     ):
-        self.filename = data_source_filename
         self.problem_type = problem_type
-        self.dataframe = self.__create_dataframe()
+
+        self.dataset_name = dataset_name
+        self.dataset_type = dataset_type
+        self.dataset_filename = self.__get_filename_by_dataset_type()
+        self.dataframe = self.__create_dataframe(self.dataset_filename)
+
         self.y_target_columns = self.__treat_y_target_columns(y_target_columns)
         self.columns = self.dataframe.columns.values.tolist()
 
-        self.__total_processed_data = self.__get_processed_data(self.dataframe)
-        self.test_data_percentage: float = 0.2
-        self.validation_data_percentage: float = 0.2
-        self.validation_data = None
-        self.training_data = None
-        self.test_data = None
-        if auto_process:
-            self.get_processed_data_slices(persist_to_csv=persist_to_csv)
+        self.processed_data = self.__get_processed_data(self.dataframe)
 
-    def __create_dataframe(self) -> pd.DataFrame:
+    def __get_filename_by_dataset_type(self):
+        slice_information = f'_{self.dataset_type}_slice' if self.dataset_type != 'complete' else ''
+        return f'{self.dataset_name}_converted{slice_information}.csv'
+
+    def __create_dataframe(self, csv_file: str) -> pd.DataFrame:
+        slice_path = 'slices/' if self.dataset_type != 'complete' else ''
         return pd.read_csv(
-            filepath_or_buffer=f'{Constants.DATA_DIRECTORY}/{self.__get_problem_type_data_path()}/{self.filename}',
+            filepath_or_buffer=f'{Constants.DATA_DIRECTORY}/{self.__get_problem_type_data_path()}/{slice_path}{csv_file}',
             header=0,
             delimiter=Constants.CSV_DELIMITER,
         )
@@ -50,37 +51,45 @@ class DataHandler:
     def __treat_y_target_columns(self, y_target_columns: str | list[str]) -> list[str]:
         return y_target_columns if isinstance(y_target_columns, list) else [y_target_columns]
 
-    def get_processed_data_slices(self, persist_to_csv: bool = False) -> tuple[
+    def get_processed_data_slices(
+            self,
+            test_set_percentage: float,
+            validation_set_percentage: float,
+            persist_to_csv: bool = False,
+    ) -> tuple[
         tuple[list[list[float]], dict[str, list[float]]],  # validation
         tuple[list[list[float]], dict[str, list[float]]],  # train
         tuple[list[list[float]], dict[str, list[float]]]  # test
     ]:
+        if not all([self.dataset_type == 'complete', 'converted' in self.dataset_filename]):
+            raise Exception('Dataset type and must be "complete" to create slices.')
+
         shuffled_dataframe = self.dataframe.sample(frac=1, random_state=42).reset_index(drop=True)
-        test_data_rows = math.ceil(shuffled_dataframe.shape[0] * self.test_data_percentage)
+        test_data_rows = math.ceil(shuffled_dataframe.shape[0] * test_set_percentage)
 
         test_dataframe = self.dataframe.iloc[:test_data_rows]
         # Temporary Training data slice without extracting Validation
         temp_training = self.dataframe.iloc[test_data_rows:]
 
         # Get Validation and Training Dataframe slices
-        validation_rows = math.ceil(temp_training.shape[0] * self.validation_data_percentage)
+        validation_rows = math.ceil(temp_training.shape[0] * validation_set_percentage)
         training_dataframe = temp_training.iloc[validation_rows:]
         validation_dataframe = temp_training.iloc[:validation_rows]
 
         # Persist csv:
         if persist_to_csv:
-            original_csv_filename = self.filename.split('.')[0]
+            original_csv_filename = self.dataset_filename.split('.')[0]
             common_path = f'{Constants.DATA_DIRECTORY}/{self.__get_problem_type_data_path()}/slices'
             validation_dataframe.to_csv(f'{common_path}/{original_csv_filename}_validation_slice.csv', index=False)
-            training_dataframe.to_csv(f'{common_path}/{original_csv_filename}_training_slice.csv',index=False)
-            test_dataframe.to_csv(f'{common_path}/{original_csv_filename}_test_slice.csv',index=False)
+            training_dataframe.to_csv(f'{common_path}/{original_csv_filename}_training_slice.csv', index=False)
+            test_dataframe.to_csv(f'{common_path}/{original_csv_filename}_test_slice.csv', index=False)
 
         # Process Dataframes slices
-        self.validation_data = self.__get_processed_data(validation_dataframe)
-        self.training_data = self.__get_processed_data(training_dataframe)
-        self.test_data = self.__get_processed_data(test_dataframe)
+        validation_processed_data = self.__get_processed_data(validation_dataframe)
+        training_processed_data = self.__get_processed_data(training_dataframe)
+        test_processed_data = self.__get_processed_data(test_dataframe)
 
-        return self.validation_data, self.training_data, self.test_data
+        return validation_processed_data, training_processed_data, test_processed_data
 
     def __get_processed_data(self, df: pd.DataFrame) -> tuple[list[list[float]], dict[str, list[float]]]:
         # Get normalized inputs and targets
